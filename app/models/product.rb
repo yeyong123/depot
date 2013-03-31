7d4513d25579f96c0ac0942a2ca0313671f54e43
yeyong14 <yeyong14@gmail.com>
2013-03-31 21:18:29 +0800

    测试

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 #encoding:UTF-8
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image_url, :price, :title
+  attr_accessible :title, :description, :image_url, :price
+  validates :title, :description, :image_url, :presence => true
+  validates :price, :numericality => { :greater_than_or_equal_to => 0.01 }
+  validates :title, :uniqueness => true
+  validates :image_url, :format => {
+            :with => %r{\.(gif|jpg|png)$}i,
+            :message => "必须是GIF 或者JPG和PNG的图像!"
+  }
 end
