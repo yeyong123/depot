f6610b03415e06d12c56b6c54c78cc2187faafac
yeyong14 <yeyong14@gmail.com>
2013-03-31 14:26:56 +0800

    完成商城的框架生成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '0581bf7b85d297815bd325e8f70404c573d2d147e18272768b3db8db2b7eee0248d0db3447abe664e140ff0d0d4b0ea515d4189e99b3fb52af14e004d8d7bc9c'
